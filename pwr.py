d2462940e7fa1de6fdbb3a25f6da9233a7c2dc40
Jason J. Gullickson <mr@jasongullickson.com>
2020-11-14 09:15:05 -0600

    Re-implemented the computations from the old script.

--- pwr.py
+++ pwr.py
@@ -1,8 +1,14 @@
-class PWR:
+# Constants
+# By default all parameters range between 0-100%,
+# but some components only function within a 
+# subset of this percentage range
+STEAM_GENERATOR_EFFICIENCY = 0.5
+GENERATOR_EFFICIENCY = 0.75
+MIN_STEAM_GENERATOR_TEMP = 25
+MIN_TURBINE_PRESSURE = 10
+MIN_GENERATOR_RPM = 20
 
-    MIN_STEAM_GENERATOR_TEMP = 25
-    MIN_TURBINE_PRESSURE = 10
-    MIN_GENERATOR_RPM = 20
+class PWR:
 
     def __init__(self, ambient_temp):
         self.ambient_temp = ambient_temp
@@ -20,24 +26,47 @@ class PWR:
         self.condenser_pump_rpm = 0
         self.condenser_temp = self.ambient_temp
         self.turbine_rpm = 0
-        generator_current = 0
+        self.generator_current = 0
 
     def tick(self):
-        simulation_time++
+        self.simulation_time += 1
         
         # Update rod position
+        # TODO: Don't let rods go out of range (0-100%)
         if self.rod_position < self.target_rod_position:
-            self.rod_position++
+            self.rod_position += 1
         if self.rod_position > self.target_rod_position:
-            self.rod_position--
+            self.rod_position -= 1
 
-        # TODO: Compute primary temp
+        # Compute primary temp
+        # TODO: Don't let temp go below ambient temp
+        # TODO: Set alarm if temp exceeds 100%
+        # TODO: Compensate for fuel depletion
+        self.primary_temp = self.primary_temp + self.rod_position
+        self.primary_temp = self.primary_temp - self.primary_pump_rpm
+
         # TODO: Compute primary pressure
+
         # TODO: Compute secondary temp
-        # TODO: Compute secondary pressure
+
+        # Compute secondary pressure
+        # TODO: Set alarm if pressure exceeds 100%
+        if self.primary_temp > MIN_STEAM_GENERATOR_TEMP:
+            self.secondary_pressure += self.primary_temp * (STEAM_GENERATOR_EFFICIENCY) / (100 - self.primary_pump_rpm)
+
         # TODO: Compute condenser temp
-        # TODO: Compute turbine RPM
-        # TODO: Compute generator current
+
+        # Compute turbine RPM
+        # TODO: Set alarm if RPM exceeds 100%
+        if self.secondary_pressure > MIN_TURBINE_PRESSURE:
+            self.turbine_rpm = round(self.secondary_pressure)
+        else:
+            self.turbine_rpm = 0
+
+        # Compute generator current
+        # TODO: Set alarm if current exceeds 100%
+        if self.turbine_rpm > MIN_GENERATOR_RPM:
+            self.generator_current = self.turbine_rpm * GENERATOR_EFFICIENCY
 
     def set_rod_position(self, position):
         self.target_rod_position = position
