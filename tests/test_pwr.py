7746df226148b43d2f24510157a23b2fd80bdadd
Jason J. Gullickson <mr@jasongullickson.com>
2020-11-18 14:41:47 -0600

    Reinitialize reactor for each test.

--- tests/test_pwr.py
+++ tests/test_pwr.py
@@ -2,23 +2,25 @@ import pytest
 
 import homer.pwr as pwr
 
-reactor = pwr.PWR(55)
-
 def test_tick():
+    reactor = pwr.PWR(55)
     reactor.tick()
     assert(reactor.simulation_time == 1)
 
 def test_rod_position():
+    reactor = pwr.PWR(55)
     reactor.set_rod_position(25)
     reactor.tick()
     assert(reactor.rod_position == 1)
 
 def test_primary_pump():
+    reactor = pwr.PWR(55)
     reactor.set_primary_pump_rpm(20)
     reactor.tick()
     assert(reactor.primary_pump_rpm == 20)
 
 def test_primary_relief_valve():
+    reactor = pwr.PWR(55)
     reactor.open_primary_relief_valve()
     reactor.tick()
     assert(reactor.primary_relief_valve)
@@ -27,11 +29,13 @@ def test_primary_relief_valve():
     assert(reactor.primary_relief_valve == False)
 
 def test_secondary_pump():
+    reactor = pwr.PWR(55)
     reactor.set_secondary_pump_rpm(25)
     reactor.tick()
     assert(reactor.secondary_pump_rpm == 25)
 
 def test_secondary_relief_valve():
+    reactor = pwr.PWR(55)
     reactor.open_secondary_relief_valve()
     reactor.tick()
     assert(reactor.secondary_relief_valve)
@@ -40,19 +44,20 @@ def test_secondary_relief_valve():
     assert(reactor.secondary_relief_valve == False)
 
 def test_condenser_pump():
+    reactor = pwr.PWR(55)
     reactor.set_condenser_pump_rpm(69)
     reactor.tick()
     assert(reactor.condenser_pump_rpm == 69)
 
 def test_scram():
+    reactor = pwr.PWR(55)
     reactor.scram()
     reactor.tick()
     assert(reactor.rod_position == 0)
 
 # Rod position should never go out of range (0-100)
 def test_rod_range():
-    reactor.set_rod_position(0)
-    reactor.tick()
+    reactor = pwr.PWR(55)
     reactor.set_rod_position(101)
     for i in range(0,105):
         reactor.tick()
